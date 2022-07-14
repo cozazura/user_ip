732ebf653b866747ed1958abf626f93e90cf5888
cozazura <90702129+cozazura@users.noreply.github.com>
2022-07-14 10:43:37 +0300

    a

--- creare_user.py
+++ creare_user.py
@@ -1,8 +1,20 @@
 #! /usr/bin/env python
 
+import paramiko
+print( "Introduceti numele pentru user-ul pe care doriti sa-l creati: " )
+user= str(input())
+print( "Introduceti parola pentru user-ul pe care doriti sa-l creati: " )
+parola= str(input())
 with open('ip.txt') as f:
-    ip = f.readlines()
+    ip = f.read().splitlines()
+    n = sum(1 for line in f)
+for i in range(1,n):
+    print( "nume user pentru ip ", ip[i] , ":")
+    nume_c= str(input())
+    print( "parola pentru ip ", ip[i] , ":")
+    parola_c= str(input())
 
-print (ip[1])
-print (ip[3])
-print (ip[2])
+    ssh = paramiko.SSHClient()
+    ssh.connect(ip[i], username=user, password=parola)
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(sudo useradd -p $(openssl passwd -1 parola_c) nume_c)
+
